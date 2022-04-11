369947326e24a83fcd117483b7c985f1809a3cb6
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-11 16:23:32 +0530

    added empty project file with welcome message

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Stacks and Queues Data Structure Problems.\n");
+        }
+    }
+}
