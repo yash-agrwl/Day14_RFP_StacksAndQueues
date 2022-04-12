a24bfc40f674c68f751107fec098ff890610d652
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-12 19:52:31 +0530

    added UC3: Ability to create a Queue and add elements to it

---
+++ StacksAndQueues/CustomQueue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    /// <summary>
+    /// Represents a first-in-first-out(FIFO) collection of instances of the same specified type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class CustomQueue<T>
+    {
+        /// <summary>
+        /// A node class reference pointing to the first element of the queue or null if the stack is empty.
+        /// </summary>
+        Node<T> Head = null;
+
+        /// <summary>
+        /// Inserts an object at the end of the <see cref="CustomQueue{T}"/>.
+        /// </summary>
+        /// <param name="data"></param>
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data); // Creates a new object of Node class.
+
+            if (Head == null) // Checks if the Head is pointing to null.
+                Head = node;
+            else
+            {
+                Node<T> temp = this.Head; // Makes the temp to point to the first element in the Queue.
+
+                while (temp.Next != null) // Loops untill temp points to the last element in the Queue.
+                    temp = temp.Next;
+
+                temp.Next = node; // Appends the node to the end of the Queue.
+            }
+
+            Console.WriteLine("{0} inserted into Queue", node.Data);
+        }
+
+        /// <summary>
+        /// Prints the elements of the <see cref="CustomQueue{T}"/> into the Console from first to last.
+        /// </summary>
+        public void Display()
+        {         
+            if (this.Head == null) // Checks if the Queue is empty or not.
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+
+            Node<T> temp = this.Head; // Makes the temp to point to the first element in the Queue.
+            Console.WriteLine("The Elements in the Queue are:");
+
+            while (temp != null) // Loops untill temp points to null.
+            {
+                Console.Write(temp.Data + " "); // Prints the data of the node pointed by the temp.
+                temp = temp.Next; // Makes temp to point to the next element in the Queue.
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -9,26 +9,56 @@ namespace StacksAndQueues
             Console.WriteLine("\tWelcome to the Stacks and Queues Data Structure Problems");
             Console.WriteLine("=========================================================================\n");
 
-            CustomStack<int> stack = new CustomStack<int>(); // Creates an instance of the CustomStack class.
+            Console.WriteLine("Press '1' to perform operations on Stack.");
+            Console.WriteLine("Press '2' to perform operations on Queue.");
+            Console.Write("Enter Choice: ");
+            string choice = Console.ReadLine();
+            Console.WriteLine();
 
-            Console.WriteLine("\tPush Method Call");
-            Console.WriteLine("----------------------------------\n");
-            // Calling Push method to add elements on top of the stack.
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
+            switch (choice)
+            {
+                case "1":
+                    CustomStack<int> stack = new CustomStack<int>(); // Creates an instance of the CustomStack class.
 
-            Console.WriteLine("\n    After Calling Push Method");
-            Console.WriteLine("----------------------------------\n");
-            stack.Display(); // Calling Display method to print all the elements in the stack.
+                    Console.WriteLine("\tPush Method Call");
+                    Console.WriteLine("----------------------------------\n");
+                    // Calling Push method to add elements on top of the stack.
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
 
-            Console.WriteLine("\tEmpty Method Call");
-            Console.WriteLine("----------------------------------\n");
-            stack.IsEmpty(); // Calling Empty method to pop out all the elements of the stack.
+                    Console.WriteLine("\n    After Calling Push Method");
+                    Console.WriteLine("----------------------------------\n");
+                    stack.Display(); // Calling Display method to print all the elements in the stack.
 
-            Console.WriteLine("\n    After Calling Empty Method");
-            Console.WriteLine("----------------------------------\n");
-            stack.Display(); // Calling Display method to print stack is empty.
+                    Console.WriteLine("\tEmpty Method Call");
+                    Console.WriteLine("----------------------------------\n");
+                    stack.IsEmpty(); // Calling Empty method to pop out all the elements of the stack.
+
+                    Console.WriteLine("\n    After Calling Empty Method");
+                    Console.WriteLine("----------------------------------\n");
+                    stack.Display(); // Calling Display method to print stack is empty.
+                    break;
+
+                case "2":
+                    CustomQueue<int> queue = new CustomQueue<int>();
+
+                    Console.WriteLine("\tEnqueue Method Call");
+                    Console.WriteLine("------------------------------------\n");
+                    // Calling Enqueue method to add element to the end of queue.
+                    queue.Enqueue(56);
+                    queue.Enqueue(30);
+                    queue.Enqueue(70);
+
+                    Console.WriteLine("\n    After Calling Enqueue Method");
+                    Console.WriteLine("------------------------------------\n");
+                    queue.Display(); // Calling Display method to print all the elements of the queue.
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Choice entered.");
+                    break;
+            }
         }
     }
 }
