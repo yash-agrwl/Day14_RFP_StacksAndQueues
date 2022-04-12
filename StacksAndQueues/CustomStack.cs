60ff55e46b18973026e1a48abf8d1aa8f3611ce9
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-12 13:59:50 +0530

    added UC2: Ability to peak and pop from the stack till it is empty

--- StacksAndQueues/CustomStack.cs
+++ StacksAndQueues/CustomStack.cs
@@ -25,15 +25,15 @@ namespace StacksAndQueues
         /// <param name="value"></param>
         public void Push(T value)
         {
-            Node<T> node = new Node<T>(value); //Creates a new object of Node class.
+            Node<T> node = new Node<T>(value); // Creates a new object of Node class.
 
-            if (this.Top == null) //Checks if the Top is pointing to null.
+            if (this.Top == null) // Checks if the Top is pointing to null.
                 node.Next = null; //Makes the node to point to null if Top is pointing to null.
             else
-                node.Next = this.Top; //Makes the node to point to the address that is in the Top.
+                node.Next = this.Top; // Makes the node to point to the address that is in the Top.
 
-            this.Top = node; //Assigns the address of the node object to the "Top" field.
-            Console.WriteLine("{0} pushed to stack", value); //Prints a success message in the console.
+            this.Top = node; // Assigns the address of the node object to the "Top" field.
+            Console.WriteLine("{0} pushed to stack", value); // Prints a success message in the console.
         }
 
         /// <summary>
@@ -41,20 +41,60 @@ namespace StacksAndQueues
         /// </summary>
         public void Display()
         {
-            if (this.Top == null) //Checks if the stack is empty or not.
+            if (this.Top == null) // Checks if the stack is empty or not.
             {
-                Console.WriteLine("\nStack is empty");
+                Console.WriteLine("Stack is empty");
                 return;
             }
 
-            Node<T> temp = this.Top; //Makes the temp to point to the address that is in the Top.
-            Console.WriteLine("\nThe Elements in the Stack are:");
+            Node<T> temp = this.Top; // Makes the temp to point to the address that is in the Top.
+            Console.WriteLine("The Elements in the Stack are:");
 
-            //Prints all the elements of the stack one by one.
+            // Prints all the elements of the stack one by one.
             while (temp != null)
             {
                 Console.WriteLine("\t" + temp.Data);
                 temp = temp.Next;
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Prints the top-most element of the instance of the <see cref="CustomStack{T}"/> class.
+        /// </summary>
+        public void Peek()
+        {
+            if (this.Top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack", this.Top.Data);
+        }
+
+        /// <summary>
+        /// Removes the top-most element from the instance of the <see cref="CustomStack{T}"/> class.
+        /// </summary>
+        public void Pop()
+        {
+            if (this.Top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("Value popped is {0}", this.Top.Data);
+            this.Top = this.Top.Next;
+        }
+
+        /// <summary>
+        /// Prints the top-most element and deletes it untill the stack becomes empty.
+        /// </summary>
+        public void IsEmpty()
+        {
+            while (this.Top != null)
+            {
+                Peek();
+                Pop();
             }
         }
     }
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -6,17 +6,29 @@ namespace StacksAndQueues
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to the Stacks and Queues Data Structure Problems.\n");
+            Console.WriteLine("\tWelcome to the Stacks and Queues Data Structure Problems");
+            Console.WriteLine("=========================================================================\n");
 
-            CustomStack<int> stack = new CustomStack<int>(); //Creates an instance of the CustomStack class.
+            CustomStack<int> stack = new CustomStack<int>(); // Creates an instance of the CustomStack class.
 
-            //Calling Push method to add elements on top of the stack.
+            Console.WriteLine("\tPush Method Call");
+            Console.WriteLine("----------------------------------\n");
+            // Calling Push method to add elements on top of the stack.
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
 
-            //Calling Display method to print all the elements in the stack.
-            stack.Display();
+            Console.WriteLine("\n    After Calling Push Method");
+            Console.WriteLine("----------------------------------\n");
+            stack.Display(); // Calling Display method to print all the elements in the stack.
+
+            Console.WriteLine("\tEmpty Method Call");
+            Console.WriteLine("----------------------------------\n");
+            stack.IsEmpty(); // Calling Empty method to pop out all the elements of the stack.
+
+            Console.WriteLine("\n    After Calling Empty Method");
+            Console.WriteLine("----------------------------------\n");
+            stack.Display(); // Calling Display method to print stack is empty.
         }
     }
 }
